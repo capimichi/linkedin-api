4713c444d8f8681934cd1cb48691cf9e53e0eadc
Michele Capichioni <capimichi@gmail.com>
2025-03-12 20:58:28 +0100

    ,Changed type of job_id from str to int and added accessor methods.

--- linkedinapi/model/JobPostingRequest.py
+++ linkedinapi/model/JobPostingRequest.py
@@ -6,4 +6,22 @@ class JobPostingRequest(BaseModel):
     Model for job posting request parameters.
     """
     username: str
-    job_id: str
+    job_id: int
+
+    def get_username(self) -> str:
+        """
+        Get the username from the request.
+
+        Returns:
+            str: LinkedIn username
+        """
+        return self.username
+
+    def get_job_id(self) -> int:
+        """
+        Get the job ID from the request.
+
+        Returns:
+            int: LinkedIn job ID
+        """
+        return self.job_id
