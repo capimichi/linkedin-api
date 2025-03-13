5031bbe41e8e6bfdadab13568bf9c9c981431bab
Michele Capichioni <capimichi@gmail.com>
2025-03-13 18:16:50 +0100

    Fixed: Job Posting ID to be of type int

--- linkedinapi/model/JobPostingSearchCard.py
+++ linkedinapi/model/JobPostingSearchCard.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from playwright.async_api import ElementHandle
 import re
 
@@ -5,8 +7,8 @@ class JobPostingSearchCard:
     def __init__(self, element_handle: ElementHandle):
         self.element_handle = element_handle
 
-    async def get_id(self) -> str:
-        return await self.element_handle.get_attribute('data-job-id')
+    async def get_id(self) -> int:
+        return int(await self.element_handle.get_attribute('data-job-id'))
 
     async def get_title(self) -> str:
         title_element = await self.element_handle.query_selector('strong')
@@ -16,10 +18,12 @@ class JobPostingSearchCard:
         company_element = await self.element_handle.query_selector('.artdeco-entity-lockup__subtitle')
         return (await company_element.inner_text()).strip()
 
-    async def get_metadata(self) -> str:
-        metadata_element = await self.element_handle.query_selector('.job-card-container__metadata-wrapper')
-        metadata_text = await metadata_element.inner_text()
-        return metadata_text.strip()
+    async def get_metadata_items(self) -> List[str]:
+        metadata_elements = await self.element_handle.query_selector_all('.job-card-container__metadata-wrapper')
+        metadata_texts = []
+        for metadata_element in metadata_elements:
+            metadata_texts.append((await metadata_element.inner_text()).strip())
+        return metadata_texts
 
     async def get_created_at(self) -> str:
         match = re.search(r'<time datetime="(.*?)"', await self.element_handle.inner_html())
