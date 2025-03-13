e11ad61a51e063760672345d2fda3a39ea32b471
Michele Capichioni <capimichi@gmail.com>
2025-03-13 21:36:04 +0100

    Added dependency injection to CompanyService.

--- linkedinapi/service/CompanyService.py
+++ linkedinapi/service/CompanyService.py
@@ -1,8 +1,12 @@
+from injector import inject
+
 from linkedinapi.client.LinkedinClient import LinkedinClient
 from linkedinapi.model.Company import Company
 
 
 class CompanyService:
+
+    @inject
     def __init__(self, linkedin_client: LinkedinClient):
         self.linkedin_client = linkedin_client
 
