9553e244ccb855bd96d5d4bf87f3bfbe82924035
Michele Capichioni <capimichi@gmail.com>
2025-03-13 17:48:56 +0100

    Added metadata retrieval function.

--- linkedinapi/model/JobPostingSearchCard.py
+++ linkedinapi/model/JobPostingSearchCard.py
@@ -16,6 +16,11 @@ class JobPostingSearchCard:
         company_element = await self.element_handle.query_selector('.artdeco-entity-lockup__subtitle')
         return (await company_element.inner_text()).strip()
 
+    async def get_metadata(self) -> str:
+        metadata_element = await self.element_handle.query_selector('.job-card-container__metadata-wrapper')
+        metadata_text = await metadata_element.inner_text()
+        return metadata_text.strip()
+
     async def get_created_at(self) -> str:
         match = re.search(r'<time datetime="(.*?)"', await self.element_handle.inner_html())
         return match.group(1) if match else None
