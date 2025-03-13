92a28ce7586674649759cbe8d0997be2e7f84f1a
Michele Capichioni <capimichi@gmail.com>
2025-03-13 21:08:36 +0100

    Added linkedinapi/service/CompanyService.py

---
+++ linkedinapi/service/CompanyService.py
@@ -0,0 +1,10 @@
+from linkedinapi.client.LinkedinClient import LinkedinClient
+from linkedinapi.model.Company import Company
+
+
+class CompanyService:
+    def __init__(self, linkedin_client: LinkedinClient):
+        self.linkedin_client = linkedin_client
+
+    async def get_company(self, username: str, company_slug: str) -> Company:
+        return await self.linkedin_client.get_company(username, company_slug)
